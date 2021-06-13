33a48fd9ed850c61bc3314fcc5e6ce6e253121ed
KuoHsiangYu2 <bbb105013@nptu.edu.tw>
2021-06-14 07:45:35 +0800

    fix bug：總欄位checkbox 點擊時無法自動全部勾選所有 checkbox。

--- src/main/webapp/js/tableData.js
+++ src/main/webapp/js/tableData.js
@@ -1,7 +1,7 @@
 console.log('tableData.js');
 
 /* 當使用者按下『儲存勾選列之背景顏色』按鈕 時，執行這段程式 */
-jQuery('#saveDataButton').on('click', function (event) {
+jQuery('#saveDataButton').on('click', function(event) {
     let selectCheckboxArray = jQuery('#CustomerTable input:checkbox[name="selectCheckbox"]:checked');
     if (selectCheckboxArray.length === 0) {
         /* 如果使用者未勾選任何 checkbox 則回傳錯誤訊息，請使用者點選。 */
@@ -12,7 +12,7 @@ jQuery('#saveDataButton').on('click', function (event) {
     let rowDataArray = [];
 
     /* 針對有勾選的[列]做處理，把該列的顏色資訊抓出來。 */
-    selectCheckboxArray.each(function () {
+    selectCheckboxArray.each(function() {
         let customerObjKey = jQuery(this).val();
         let backgroundColor = jQuery(this).parents('tr').find('option:selected').val();
 
@@ -29,58 +29,54 @@ jQuery('#saveDataButton').on('click', function (event) {
 
     /* 接著使用AJAX技術，由JavaScript發送POST請求輸出資料到後端程式。 */
     jQuery.ajax({
-        'type': 'POST',
-        'url': pageContext + '/SaveRowColorData',
+        type: 'POST',
+        url: pageContext + '/SaveRowColorData',
         'Content-type': 'application/x-www-form-urlencoded',
-        'If-Modified-Since': '0',
-        'data': JSON.stringify(rowDataArray),
-        'success': function (response) {
+        data: JSON.stringify(rowDataArray),
+        success: function(response) {
             console.log('response = ', response);
             if (response === 'isOK') {
                 /* 前端發送POST請求，後端回應一個response，成功後會執行此段程式。 */
                 window.location.reload();
-            } else if (this.readyState == 4) {
+            } else {
                 window.alert('儲存資料失敗。');
             }
         },
-        'error': function (thrownError) {
+        error: function(thrownError) {
             console.log('thrownError = ', thrownError);
             window.alert('儲存資料失敗。');
         }
     });
 });
 
-jQuery('#allCheckbox1').on('click', function (event) {
+jQuery('#allCheckbox1').on('click', function(event) {
     /* 全選所有 checkbox */
+    /* https://reurl.cc/xGQVEV */
     let allCheckboxArray = jQuery('#CustomerTable input:checkbox[name="selectCheckbox"]');
     let length = allCheckboxArray.length;
-    let checkStatus = allCheckbox1Obj.checked;
+    let checkStatus = jQuery('#allCheckbox1').prop('checked');
     if (true === checkStatus) {
-        for (let i = 0; i < length; i++) {
-            allCheckboxArray.get(i).checked = true;
-        }
+        jQuery('#CustomerTable input:checkbox[name="selectCheckbox"]').prop('checked', true);
     } else {
-        for (let i = 0; i < length; i++) {
-            allCheckboxArray.get(i).checked = false;
-        }
+        jQuery('#CustomerTable input:checkbox[name="selectCheckbox"]').prop('checked', false);
     }
 });
 
 /* 設定預設 */
-jQuery('#CustomerTable .theSelect').each(function () {
+jQuery('#CustomerTable .theSelect').each(function() {
     let defaultColor = jQuery(this).find('option:selected').val();
     jQuery(this).css('backgroundColor', defaultColor);
     jQuery(this).parents('tr').css('backgroundColor', defaultColor);
 });
 
 /* option標籤顏色 */
-jQuery('#CustomerTable .theSelect').find('option').each(function () {
+jQuery('#CustomerTable .theSelect').find('option').each(function() {
     let color = jQuery(this).val();
     jQuery(this).css('backgroundColor', color);
 });
 
 /* 變更option改變背景顏色 */
-jQuery('#CustomerTable .theSelect').change(function () {
+jQuery('#CustomerTable .theSelect').change(function() {
     let selectedItem = jQuery(this).find('option:selected');
     jQuery(this).css('backgroundColor', selectedItem.val());
     jQuery(this).parents('tr').css('backgroundColor', selectedItem.val());
